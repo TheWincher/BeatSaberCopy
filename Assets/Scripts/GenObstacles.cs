c572f99d6f6c5f58ae82406282b58ba33700bcf9
Lupusanghrenn <brisingr34130@gmail.com>
2020-01-17 16:51:00 +0100

    Premier push pour le jeu

---
+++ Assets/Scripts/GenObstacles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenObstacles : MonoBehaviour
+{
+
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("GenObs", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenObs()
+    {
+        GameObject obs = Resources.Load<GameObject>("Obstacles/obstacles");
+        Material mat = Resources.Load<Material>("Materials/Red");
+        Instantiate(obs,transform);
+        Invoke("GenObs", timer);
+    }
+}
