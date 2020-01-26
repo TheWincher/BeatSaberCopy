dd507edde8a9af8d07685ada6340f50d9a1af9fd
Homère <homerebourgeois@hotmail.fr>
2020-01-26 20:48:25 +0100

    Debut implémentation des cibles et obstacles

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    public Collider trigger;
+    public float activationTimer;
+    public float maxActivationTimer;
+    public float activeTimer;
+    public float maxActiveTimer;
+    public GameObject activationImage;
+    public float activationImageMinScale;
+    Material imageMaterial;
+    Material objectMaterial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectMaterial = GetComponent<Renderer>().material;
+
+
+        float activationImageScale = activationImageMinScale + (activationTimer / maxActivationTimer);
+        activationImage.transform.localScale = new Vector3(activationImageScale, activationImageScale, activationImageScale);
+        imageMaterial = activationImage.GetComponent<Renderer>().material;
+        imageMaterial.color = new Color(1.0f/activationImageScale, 0, 0, 1.0f/activationImageScale);
+        StartCoroutine("DestroyAfterTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activationTimer -= Time.deltaTime;
+        if (activationTimer > 0)
+        {
+            float activationImageScale = activationImageMinScale + (activationTimer / maxActivationTimer);
+            activationImage.transform.localScale = new Vector3(activationImageScale, activationImageScale, activationImageScale);
+            imageMaterial.color = new Color(1.0f / activationImageScale, 0, 0, 1.0f / activationImageScale);
+        }
+        else
+        {
+            activeTimer -= Time.deltaTime;
+            objectMaterial.color = new Color(activeTimer / maxActiveTimer, 0, 0, activeTimer / maxActiveTimer);
+            objectMaterial.SetColor("_EmissionColor",new Color(activeTimer/maxActiveTimer, 0, 0));
+        }
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(maxActivationTimer);
+        Destroy(activationImage);
+        objectMaterial.color = (Color.red);
+        objectMaterial.EnableKeyword("_EMISSION");
+        objectMaterial.SetColor("_EmissionColor", Color.red);
+
+        yield return new WaitForSeconds(maxActiveTimer);
+        Destroy(gameObject);
+    }
+}
